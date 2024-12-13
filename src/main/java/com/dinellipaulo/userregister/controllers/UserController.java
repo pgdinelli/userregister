a6d7f270ddd33be1ca043ab1576435aaf24d314a
dinellipaulo <pgsdinelli@gmail.com>
2024-12-13 02:42:07 -0300

    Implemented methods find all users and find users by id + http requests

--- src/main/java/com/dinellipaulo/userregister/controllers/UserController.java
+++ src/main/java/com/dinellipaulo/userregister/controllers/UserController.java
@@ -1,5 +1,32 @@
 package com.dinellipaulo.userregister.controllers;
 
-public class UserController {
+import com.dinellipaulo.userregister.entities.User;
+import com.dinellipaulo.userregister.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> findAllUsers(){
+        return userService.findAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<User> findUserById(@PathVariable Long id){
+        Optional<User> user = userService.findUsersById(id);
+        return user;
+    }
+
+    @PostMapping
+    public User createUser(User user){
+        return userService.createUser(user);
+    }
 }
--- src/main/java/com/dinellipaulo/userregister/services/UserService.java
+++ src/main/java/com/dinellipaulo/userregister/services/UserService.java
@@ -5,11 +5,22 @@ import com.dinellipaulo.userregister.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public List<User> findAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public Optional<User> findUsersById(Long id){
+        return userRepository.findById(id);
+    }
 
     public User createUser(User user){
         return userRepository.save(user);
