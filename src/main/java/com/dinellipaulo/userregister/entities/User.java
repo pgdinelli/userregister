ec46f40e6620717edb81268cc534139019f550e9
dinellipaulo <pgsdinelli@gmail.com>
2024-12-13 02:00:32 -0300

    Updated application.properties file, implemented create user service, and table annotation on user class

--- src/main/java/com/dinellipaulo/userregister/entities/User.java
+++ src/main/java/com/dinellipaulo/userregister/entities/User.java
@@ -1,13 +1,11 @@
 package com.dinellipaulo.userregister.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_user")
 public class User {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
--- src/main/java/com/dinellipaulo/userregister/services/UserService.java
+++ src/main/java/com/dinellipaulo/userregister/services/UserService.java
@@ -1,4 +1,17 @@
 package com.dinellipaulo.userregister.services;
 
+import com.dinellipaulo.userregister.entities.User;
+import com.dinellipaulo.userregister.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
 }
